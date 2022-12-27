90aaf8dc5a49301adb23c06f7a100c1b74022b55
Saunved <saunved@gmail.com>
2022-12-28 04:25:31 +0530

    Create a basic sidebar

---
+++ components/HomeSidebar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function HomeSidebar() {
+  return (
+    <div className="border p-4 rounded-md" style={{ minHeight: "250px" }}>
+      <section className="mb-4">
+        <p className="text-sm uppercase tracking-wider">Categories</p>
+        <p className="text-xs">To be added</p>
+      </section>
+
+      <section>
+        <p className="text-sm uppercase tracking-wider">Popular content</p>
+        <p className="text-xs">To be added</p>
+      </section>
+    </div>
+  );
+}
+
+export default HomeSidebar;
