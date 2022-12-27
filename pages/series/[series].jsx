5344cb99ef5ada4014d8cbb2766b7a2fe484e5b2
Saunved <saunved@gmail.com>
2022-12-28 04:33:48 +0530

    Create a dynamic series page
    + All posts of a particular series will be viewable on
      this dynamic page

---
+++ pages/series/[series].jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import PropTypes from "prop-types";
+import GenericPostFeed from "../../components/GenericPostFeed";
+import { importAll } from "../../helpers/importAll";
+import { meta } from "../../constants/propTypes";
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const articles = await importAll("");
+
+  const articlesMeta = articles
+    .map((article) => (article?.meta ? article.meta : null))
+    .filter(
+      (_meta) =>
+        _meta?.categories.includes("series") && _meta.seriesId === params.series
+    );
+
+  return {
+    props: {
+      articles: articlesMeta,
+      series:
+        articlesMeta && articlesMeta.length
+          ? articlesMeta[0].series
+          : params.series,
+    },
+  };
+}
+
+function PostSeries({ articles, series }) {
+  return (
+    <GenericPostFeed
+      postsMeta={articles}
+      title={`${series} (${articles.length} parts)`}
+    />
+  );
+}
+
+PostSeries.propTypes = {
+  articles: PropTypes.arrayOf(meta).isRequired,
+  series: PropTypes.string.isRequired,
+};
+
+export default PostSeries;
