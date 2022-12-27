aa37326e7e34b55886aab69ecad1e6d53327d28d
Saunved <saunved@gmail.com>
2022-12-28 04:25:43 +0530

    Create a generic post feed component
    + This is essentially a wrapper around PostList along
      with the HomeSidebar
    + This component is used to render all sorts of posts,
      the source of the post being determined via params and
      the files fetched from the filesystem at runtime (if required)

---
+++ components/GenericPostFeed.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import HomeSidebar from "./HomeSidebar";
+import PostList from "./PostList";
+import { meta } from "../constants/propTypes";
+
+function GenericPostFeed({ title, postsMeta }) {
+  return (
+    <div className="md:grid grid-cols-12 mt-12 gap-12">
+      <div className="col-span-8">
+        <h2 className="text-xl uppercase mb-4 tracking-wider font-bold">
+          {title}
+        </h2>
+        <section className="md:grid grid-cols-2 gap-4">
+          {!postsMeta || !postsMeta.length ? (
+            <div>No posts found for this category</div>
+          ) : (
+            <PostList posts={postsMeta} />
+          )}
+        </section>
+      </div>
+      <div className="col-span-4">
+        <HomeSidebar />
+      </div>
+    </div>
+  );
+}
+
+GenericPostFeed.propTypes = {
+  title: PropTypes.string.isRequired,
+  postsMeta: PropTypes.arrayOf(meta).isRequired,
+};
+
+export default GenericPostFeed;
