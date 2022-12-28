51ea5d1d46d05e9530f8aa40c59867fd1270f974
Saunved <saunved@gmail.com>
2022-12-29 02:56:17 +0530

    Add breadcrumbs to top level pages

--- components/GenericPostFeed.jsx
+++ components/GenericPostFeed.jsx
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import PostList from "./PostList";
 import { meta } from "../constants/propTypes";
+import BreadCrumbs from "./BreadCrumbs";
 
 function GenericPostFeed({ title, postsMeta, cardType = "standalone" }) {
+  const [breadCrumbLinks, setBreadCrumbLinks] = useState([]);
+
+  useEffect(() => {
+    const getBreadCrumbLinks = () => {
+      if (document.location.pathname === "/") {
+        return [];
+      }
+
+      const postIsFromSeries = document.location.pathname.includes("/series/");
+
+      const links = [
+        {
+          href: "/",
+          title: "Home",
+        },
+      ];
+
+      if (postIsFromSeries) {
+        links.push({
+          href: "/series",
+          title: "Series",
+        });
+      }
+
+      links.push({
+        href: document.location.href,
+        title,
+        disabled: true,
+      });
+
+      return links;
+    };
+
+    setBreadCrumbLinks(getBreadCrumbLinks());
+  }, [title]);
+
   return (
     <div>
+      <BreadCrumbs links={breadCrumbLinks} />
       <h2 className="text-2xl mb-4 dark:text-gray-200 font-semibold">
         {title}
       </h2>
