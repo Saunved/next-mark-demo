bb8a2900523b3c086639a3ac2d1c12ce9f4fed97
Saunved <saunved@gmail.com>
2022-12-29 01:53:52 +0530

    Create a series index page

---
+++ pages/series.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { importAll } from "../helpers/importAll";
+import series from "../constants/series";
+import GenericPostFeed from "../components/GenericPostFeed";
+import { meta } from "../constants/propTypes";
+
+export async function getStaticProps() {
+  const articles = await importAll("");
+
+  const isFirstPostOfSeries = (order) => order === 1;
+
+  const articlesMeta = articles
+    .map((article) => (article?.meta ? article.meta : null))
+    .filter((_meta) => isFirstPostOfSeries(_meta.order))
+    .map((_meta) => {
+      // eslint-disable-next-line no-underscore-dangle
+      const _series = series.find((serie) => serie.seriesId === _meta.seriesId);
+      if (_series) {
+        return {
+          ..._meta,
+          title: _series.series,
+          description: _series.description,
+          slug: `/series/${_series.seriesId}`,
+          parts: articles.filter(
+            (article) => article?.meta.seriesId === _series.seriesId
+          ).length,
+        };
+      }
+
+      return null;
+    })
+    .filter((_meta) => _meta);
+
+  return {
+    props: {
+      articles: articlesMeta,
+    },
+  };
+}
+
+export default function Series({ articles }) {
+  return (
+    <GenericPostFeed
+      postsMeta={articles}
+      title="All series"
+      cardType="collection"
+    />
+  );
+}
+
+Series.propTypes = {
+  articles: PropTypes.arrayOf(meta).isRequired,
+};
