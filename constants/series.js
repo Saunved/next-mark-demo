4b7f7598b3eada100a5f59b4d6983568627d17a6
Saunved <saunved@gmail.com>
2022-12-28 15:40:38 +0530

    Add description for the "Other Access" series

--- constants/series.js
+++ constants/series.js
@@ -2,4 +2,6 @@
 export const otherAccess = {
   seriesId: "other-access",
   series: "Other access",
+  description:
+    'Pyrel Sesta is three days away from being captured by the government and put to "sleep" forever. A mysterious stranger, and an old family secret could help him escape.',
 };
