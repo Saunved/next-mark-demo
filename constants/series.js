d1f62abe3e85681c4aa0fa6633593dd832360319
Saunved <saunved@gmail.com>
2022-12-29 01:53:34 +0530

    Add info about Adventures in Seychelles series

--- constants/series.js
+++ constants/series.js
@@ -1,7 +1,16 @@
 // eslint-disable-next-line import/prefer-default-export
-export const otherAccess = {
-  seriesId: "other-access",
-  series: "Other access",
-  description:
-    'Pyrel Sesta is three days away from being captured by the government and put to "sleep" forever. A mysterious stranger, and an old family secret could help him escape.',
-};
+
+export default [
+  {
+    seriesId: "other-access",
+    series: "Other access",
+    description:
+      'Pyrel Sesta is three days away from being captured by the government and put to "sleep" forever. A mysterious stranger, and an old family secret could help him escape.',
+  },
+  {
+    seriesId: "adventures-in-seychelles",
+    series: "Adventures in Seychelles",
+    description:
+      "This series follows my adventures in Seychelles as a kid, filled with fun anecdotes and unique experiences",
+  },
+];
