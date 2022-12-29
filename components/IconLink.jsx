5ecb5a01ee34ca815a4446be77249985fa6140e5
Saunved <saunved@gmail.com>
2022-12-29 06:15:07 +0530

    Allow for icons to be added to the left side of a link

--- components/IconLink.jsx
+++ components/IconLink.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { ArrowUpRight } from "phosphor-react";
 
-function IconLink({ href, title, icon, external, iconSize }) {
+function IconLink({ href, title, icon, external, iconSize, iconLeft }) {
   const iconEl =
     icon || (external ? <ArrowUpRight size={iconSize || 16} /> : null);
 
@@ -13,7 +13,7 @@ function IconLink({ href, title, icon, external, iconSize }) {
       className="flex justify-start gap-1 items-center"
       rel="noreferrer"
     >
-      {title} {iconEl}
+      {iconLeft} {title} {iconEl}
     </a>
   );
 }
@@ -22,11 +22,13 @@ IconLink.propTypes = {
   href: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   icon: PropTypes.element,
+  iconLeft: PropTypes.element,
   external: PropTypes.bool,
   iconSize: PropTypes.number,
 };
 
 IconLink.defaultProps = {
+  iconLeft: null,
   icon: null,
   external: false,
   iconSize: 0,
