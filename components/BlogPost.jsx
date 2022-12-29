bf1ba942f968ab580fd55f64ebe076363313e5a7
Saunved <saunved@gmail.com>
2022-12-29 21:22:47 +0530

    Limit width of the article section

--- components/BlogPost.jsx
+++ components/BlogPost.jsx
@@ -68,33 +68,35 @@ export default function BlogPost({ meta, children }) {
         }}
       />
 
-      <div>
-        <BreadCrumbs links={breadCrumbLinks} />
-        <h1 className="text-4xl font-bold">{meta.title}</h1>
-        <p className="text-gray-600 dark:text-gray-300 mt-2">
-          {meta.author} &bull; {meta.date} &bull; {meta.readTime} min read
-        </p>
-        <figure>
-          <Image
-            className="mt-8 rounded-md"
-            src={meta.image}
-            height={400}
-            width={1200}
-            alt={meta.alt}
-          />
-          <figcaption>{meta.credit}</figcaption>
-        </figure>
-      </div>
-      <article className="mt-8 prose prose-neutral prose-lg dark:prose-invert">
-        {children}
-      </article>
+      <div className="max-w-2xl ml-0">
+        <div>
+          <BreadCrumbs links={breadCrumbLinks} />
+          <h1 className="text-4xl font-bold">{meta.title}</h1>
+          <p className="text-gray-600 dark:text-gray-300 mt-2">
+            {meta.author} &bull; {meta.date} &bull; {meta.readTime} min read
+          </p>
+          <figure>
+            <Image
+              className="mt-8 rounded-md"
+              src={meta.image}
+              height={400}
+              width={1200}
+              alt={meta.alt}
+            />
+            <figcaption>{meta.credit}</figcaption>
+          </figure>
+        </div>
+        <article className="mt-8 prose prose-neutral prose-lg dark:prose-invert">
+          {children}
+        </article>
 
-      <hr className="my-10 dark:border-gray-600 hidden" />
-      <section className="pt-4 pb-16 hidden">
-        <p className="text-xl font-bold">Comments</p>
-        <p className="text-xs">Coming soon</p>
-      </section>
-      <hr className="md:hidden my-10 dark:border-gray-600" />
+        <hr className="my-10 dark:border-gray-600 hidden" />
+        <section className="pt-4 pb-16 hidden">
+          <p className="text-xl font-bold">Comments</p>
+          <p className="text-xs">Coming soon</p>
+        </section>
+        <hr className="md:hidden my-10 dark:border-gray-600" />
+      </div>
     </>
   );
 }
