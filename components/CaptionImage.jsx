8e039d693ed4a8fc070e014cd3eca0d00ec2245d
Saunved <saunved@gmail.com>
2022-12-29 21:23:07 +0530

    Allow both full page and smaller images for CaptionImage

--- components/CaptionImage.jsx
+++ components/CaptionImage.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import Image from "next/image";
 import PropTypes from "prop-types";
 
-function CaptionImage({ alt, src, caption, children }) {
+function CaptionImage({
+  alt,
+  src,
+  caption,
+  children,
+  width = 400,
+  height = 400,
+  full,
+}) {
   return (
     <figure className="not-prose">
       <Image
         src={src}
         alt={alt}
-        width={800}
-        height={400}
+        width={full ? 1024 : width}
+        height={full ? 1024 : height}
+        objectFit="cover"
         className="rounded-md"
       />
       <figcaption>{caption || children}</figcaption>
@@ -22,11 +31,17 @@ CaptionImage.propTypes = {
   src: PropTypes.string.isRequired,
   caption: PropTypes.string,
   children: PropTypes.element,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  full: PropTypes.bool,
 };
 
 CaptionImage.defaultProps = {
   caption: "",
   children: null,
+  width: 400,
+  height: 400,
+  full: false,
 };
 
 export default CaptionImage;
