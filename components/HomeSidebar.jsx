daa39c6a7a792548a334ea76f266dfb871332c3c
saunved <saunved@gmail.com>
2023-06-20 02:45:27 +0530

    Add email collection form to sidebar

--- components/HomeSidebar.jsx
+++ components/HomeSidebar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import sidebar from "constants/sidebar";
 import SidebarLinks from "components/SidebarLinks";
+import EmailCollector from "components/EmailCollector";
 
 function HomeSidebar() {
   return (
@@ -8,34 +9,18 @@ function HomeSidebar() {
       className="md:p-4 dark:bg-zinc-900 bg-white overflow-auto sidebar md:sticky md:top-0"
       style={{ minHeight: "250px", maxHeight: "80vh" }}
     >
+
+      <EmailCollector className="mb-8" />
+
       {sidebar.map((section) => (
         <section className="mb-8" key={section.title}>
-          <p className="text-base tracking-wider dark:text-emerald-400 font-semibold">
+          <p className="text-base tracking-wide dark:text-emerald-400 font-semibold">
             {section.title}
           </p>
           <SidebarLinks links={section.links} />
         </section>
       ))}
 
-      <section className="hidden">
-        <p className="text-base tracking-wider dark:text-emerald-400 font-semibold">
-          Subscribe via email
-        </p>
-        <p className="text-sm">
-          Get curated weekly updates straight to your inbox
-        </p>
-        <input
-          type="email"
-          className="px-2 py-1.5 rounded-md border border-gray-500 text-lg w-full tracking-tight mt-2"
-          placeholder="Your email"
-        />
-        <button
-          type="button"
-          className="border dark:border-none dark:hover:bg-blue-700 p-1 text-lg mt-2 mb-2 w-full rounded-md bg-blue-600 text-white"
-        >
-          Subscribe
-        </button>
-      </section>
     </aside>
   );
 }
