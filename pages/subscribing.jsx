e1ba9bc8ac79285776a4b961429994ab530ebbb9
saunved <saunved@gmail.com>
2023-06-20 02:45:59 +0530

    Show this page when user needs to confirm email

---
+++ pages/subscribing.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export default function SubscribedPage() {
+    return (
+        <div className="text-center my-12 border-gray-200 rounded-xl dark:bg-gray-800 bg-gray-200 p-8">
+            <h1 className="text-xl">One last step...</h1>
+            <p className="text-md mt-4">Please check your email for a confirmation. <br />I promise an amazingly cute cat picture once you do!</p>
+            <p className="text-md mt-4">The confirmation email might take a minute or so to arrive, or it might land in your spam.</p>
+        </div>
+    )
+}
