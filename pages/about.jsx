486bff597fc4395b0bbc9a41c28a3d20840baeac
saunved <saunved@gmail.com>
2023-06-20 02:45:16 +0530

    Add an About page

---
+++ pages/about.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export default function SubscribedPage() {
+    return (
+        <div className="my-12 border-gray-200 rounded-xl bg-gray-100 dark:bg-gray-800 p-8 text-lg">
+            <h1 className="text-3xl">About me</h1>
+
+            <p className="mt-4">Hey there! I am Saunved.</p>
+
+            <p className="mt-4">
+                I write stories and articles about a wide variety of topics, ranging from personal experiences, to fictional worlds, or often times, a mix of both. I am a software engineer by profession, so I occasionally write tech guides or articles pertaining to programming.
+            </p>
+
+            <p className="mt-4">
+                By subscribing to my website, you can help me grow an independent blog that is free from the constraints and whims of any publishing platform.
+            </p>
+
+            <p className="mt-4">
+                I am also figuring out a way to make the underlying code of this website open-source (i.e. public, and free to use), so that other developers can reuse it to create their own website.
+            </p>
+        </div>
+    )
+}
