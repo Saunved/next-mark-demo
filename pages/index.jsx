6982a7f191603ae5abb8660b3d28ef74b7bee3b6
Saunved <saunved@gmail.com>
2024-06-18 02:23:34 +0530

    Update home page to display featured posts

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,29 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { importAllPostsMeta } from "helpers/importPostsMeta";
+import { importFeaturedPostsMeta } from "helpers/importPostsMeta";
 import GenericPostFeed from "components/GenericPostFeed";
 import feedTypes from "constants/feedTypes";
+import Link from "next/link";
 
 export async function getStaticProps() {
-  const posts = await importAllPostsMeta();
+  const featuredPosts = await importFeaturedPostsMeta();
 
   return {
     props: {
-      posts
+      featuredPosts
     },
   };
 }
 
-export default function Home({ posts }) {
+export default function Home({ featuredPosts }) {
   return (<div className="grid gap-12">
-    <section id="stories">
-      <GenericPostFeed postsMeta={posts} title="Recent posts" feedType={feedTypes.imageGrid} />
+    <section id="featured">
+      <GenericPostFeed postsMeta={featuredPosts} title="Featured posts" feedType={feedTypes.imageGrid} />
     </section>
+
+    <section>
+      <h2 className="text-xl">Series</h2>
+      <Link href="/tags/other-access">Other Access</Link>
+    </section>
+
   </div>
   )
 }
 
 Home.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
-  posts: PropTypes.array.isRequired,
+  featuredPosts: PropTypes.array.isRequired,
 };
