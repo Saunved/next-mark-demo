3b498e5309074db958d7ddfaaac057ca46a4bd6f
Saunved <saunved@gmail.com>
2024-06-28 00:19:49 +0530

    Render footer links conditionally

--- components/PageFooter.jsx
+++ components/PageFooter.jsx
@@ -9,65 +9,79 @@ function PageFooter({ className = "" }) {
       <div className="max-w-5xl mx-auto py-4">
         <div className={`mt-8 ${className}`}>
           <div className="grid grid-cols-4 md:grid-cols-12 gap-8">
-            <div className="col-span-4">
-              <p className="uppercase font-bold text-sm">Connect via</p>
-              <ul className="grid gap-1 mt-2 text-base">
-                {
-                  blogConfig.footer.social.map(social => (
-                    <li key={social.href}>
-                      <IconLink
-                        href={social.href}
-                        title={social.title}
-                        external={social.external}
-                      />
-                    </li>
-                  ))
-                }
-              </ul>
-            </div>
 
-            <div className="col-span-4">
-              <p className="uppercase font-bold text-sm">Platforms</p>
-              <ul className="grid gap-1 mt-2 text-base">
-                {
-                  blogConfig.footer.platforms.map(social => (
-                    <li key={social.href}>
-                      <IconLink
-                        href={social.href}
-                        title={social.title}
-                        external={social.external}
-                      />
-                    </li>
-                  ))
-                }
-              </ul>
-            </div>
+            {
+              !blogConfig.footer.social.length ? null :
+                <div className="col-span-4">
+                  <p className="uppercase font-bold text-sm">Connect via</p>
+                  <ul className="grid gap-1 mt-2 text-base">
+                    {
+                      blogConfig.footer.social.map(social => (
+                        <li key={social.href}>
+                          <IconLink
+                            href={social.href}
+                            title={social.title}
+                            external={social.external}
+                          />
+                        </li>
+                      ))
+                    }
+                  </ul>
+                </div>
+            }
 
-            <div className="col-span-4">
-              <p className="uppercase font-bold text-sm">Other links</p>
-              <ul className="grid gap-1 mt-2 text-base">
-                {
-                  blogConfig.footer.other.map(other => (
-                    <li key={other.href}>
-                      <IconLink
-                        href={other.href}
-                        title={other.title}
-                        external={other.external}
-                      />
-                    </li>
-                  ))
-                }
-              </ul>
-            </div>
+            {
+              !blogConfig.footer.platforms?.length ? null :
+                <div className="col-span-4">
+                  <p className="uppercase font-bold text-sm">Platforms</p>
+                  <ul className="grid gap-1 mt-2 text-base">
+                    {
+                      blogConfig.footer.platforms.map(social => (
+                        <li key={social.href}>
+                          <IconLink
+                            href={social.href}
+                            title={social.title}
+                            external={social.external}
+                          />
+                        </li>
+                      ))
+                    }
+                  </ul>
+                </div>
+            }
 
+            {
+              !blogConfig.footer.other.length ? null :
+                <div className="col-span-4">
+                  <p className="uppercase font-bold text-sm">Other links</p>
+                  <ul className="grid gap-1 mt-2 text-base">
+                    {
+                      blogConfig.footer.other.map(other => (
+                        <li key={other.href}>
+                          <IconLink
+                            href={other.href}
+                            title={other.title}
+                            external={other.external}
+                          />
+                        </li>
+                      ))
+                    }
+                  </ul>
+                </div>
+            }
           </div>
         </div>
 
-        <hr className="border-stone-900 dark:border-stone-500 mt-6" />
+        {
+          !blogConfig?.copyRight ? null :
+            <>
+              <hr className="border-stone-900 dark:border-stone-500 mt-6" />
 
-        <p className="text-center mt-6">
-          &copy; {blogConfig.copyRight}
-        </p>
+              <p className="text-center mt-6">
+                &copy; {blogConfig.copyRight}
+              </p></>
+        }
+
       </div>
     </footer>
   );
